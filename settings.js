ee643fbc09afdced325a775dd7ccd2d5a45b7824
Wamfy <109063791+Wamfy@users.noreply.github.com>
2022-07-19 22:46:41 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,20 +22,20 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+923049595121'] //Owner number in aive msg
+global.premium = ['+923049595121'] //Owner Number info
+global.ownernomer = '+923049595121' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'Asif Ansari' //Owner Name
+global.botname = ' 🇼‌🇦‌🇲‌🇫‌🇾‌   🇹‌🇪‌🇨‌🇭‌' //Bot Name
 
 global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.btnurl = 'https://youtube.com/channel/UCk8D4gwDRSHWE8BQ-rzZNhg?sub_confirmation=1' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '🇼‌🇦‌🇲‌🇫‌🇾‌   🇹‌🇪‌🇨‌🇭‌' //Bot Pacage Name
+global.author = 'Asif Ansari' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
